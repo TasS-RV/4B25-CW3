18a09dacc8016c1c00a6dd56e77f714a7da40a0f
TasS-RV <sayet025.309@gmail.com>
2025-03-16 05:23:01 +0000

    Seems that update buffers is causing an issue... - may just need to update from within the function

--- src/boot/ksdk1.1.0/boot.c
+++ src/boot/ksdk1.1.0/boot.c
@@ -2107,21 +2107,16 @@ main(void)
 	warpPrint("\nFinished initialising sensor.\n");
 	
 	uint16_t timeBefore = 0;
-	uint16_t timeNow = 0;
-	//uint16_t local_diff = 0;
+	uint16_t timeAft = 0;
 
 	for (int i = 0; i < 600; i++){
-		
-		local_diff = timeNow - timeBefore; // This is used to measure a non-blocking delay before polling the IMU registers
-		if (local_diff >= 500){
-			accel_mag = byte_to_state_conversion();
-			timeBefore = OSA_TimeGetMsec();
-			update_buffers(accel_mag, (uint16_t)((timeBefore - timeNow) + local_diff));	
-		} 
-		timeNow = OSA_TimeGetMsec();
-		warpPrint("Current time difference: %d \n", local_diff);
-	
-	// Manual 0.5s delay between printed readings - repeats ther cycle 600x (5 minutes)
+		//timeBefore = OSA_TimeGetMsec();
+		byte_to_state_conversion(); //Obtrain time taken to poll - Error will exist when upodating buffers
+		//timeAft = OSA_TimeGetMsec();
+		//update_buffers(accel_mag, (uint16_t)((timeBefore - timeAft) + 100));	
+		//warpPrint("Current time difference: %d \n", (timeBefore - timeAft) + 100);
+
+		// Manual 0.5s delay between printed readings - repeats ther cycle 600x (5 minutes)
 	OSA_TimeDelay(100);
 	}
 	
